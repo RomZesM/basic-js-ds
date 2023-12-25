a258d0777c7de8e0cfd3075f480c7d79bf42ddcb
Raman Marсulevich <dwarf.md@gmail.com>
2023-12-25 17:47:11 +0100

    feat: add queue

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError, ListNode } = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -14,23 +14,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
+  head = null;
+  last = null;
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   return this.head;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+   let node = new ListNode(value);
+   if(this.head === null){
+    this.head = node;
+    this.last = this.head;
+   }
+   else{
+    //add node to the linkedlist
+    this.last.next = node;
+    this.last = node;
+   }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let ret = this.head.value;
+    this.head = this.head.next;
+    return ret;
   }
 }
 
 module.exports = {
   Queue
 };
+
+
+function printLinkedList(list){
+  let head = list;
+  let current = head;
+  let arr = []
+  while(current != null){
+    arr.push(current.value)
+    current = current.next;
+  }
+  console.log(arr);
+}
+
+// let q = new Queue;
+// q.enqueue(5);
+// q.enqueue(6);
+// q.enqueue(7);
+// q.enqueue(8);
+// printLinkedList(q.head)
+// console.log(q.dequeue());
+// printLinkedList(q.head)
